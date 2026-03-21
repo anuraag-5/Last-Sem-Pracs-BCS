df12fa63684b5a4de50cf3364976374f696bc7b7
Anurag bhoite <anuragcr7778@gmail.com>
2026-03-21 22:36:58 +0530

    java ass-2

--- adv-java/ass-2/Set-A/Main.java
+++ adv-java/ass-2/Set-A/Main.java
@@ -1,17 +1,27 @@
-class SleepDemo extends Thread {
+class EvenThread implements Runnable {
     public void run() {
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            try {
-                Thread.sleep(1000);
-            } catch(Exception e) {}
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0)
+                System.out.println("Even: " + i);
+        }
+    }
+}
+
+class OddThread implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 != 0)
+                System.out.println("Odd: " + i);
         }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        SleepDemo t = new SleepDemo();
-        t.start();
+        Thread t1 = new Thread(new EvenThread());
+        Thread t2 = new Thread(new OddThread());
+
+        t1.start();
+        t2.start();
     }
 }
